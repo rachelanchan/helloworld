355116d0782a0cc071ea2ea9dcc0cc9bb597864a
rachelanchan <66233783+rachelanchan@users.noreply.github.com>
2024-01-19 17:34:40 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -7,6 +7,7 @@ class helloworld
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("Hello, World! ");
+        System.out.println("Hello, World!");
+        System.out.println("Jenkins Lab Work!");
     }
 }
