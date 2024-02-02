b0edae1d33d27bbb223ed7fab36343b3c9333234
rachelanchan <66233783+rachelanchan@users.noreply.github.com>
2024-02-02 12:18:43 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -9,6 +9,6 @@ class helloworld
     {
         System.out.println("Hello, World!");
         System.out.println("Jenkins Lab Work");
-        System.out.println("DevOps Labwork");
+        System.out.println("DevOps Labwork 2");
     }
 }
